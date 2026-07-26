6c5fcb70055205a770406efececd4049bd03d02c
Kevin Fox <foxkevin@protonmail.com>
2026-07-25 23:09:06 -0400

    added logic for part4, and exported all outputs to csv for demonstration purposes

--- csv_helper.py
+++ csv_helper.py
@@ -3,6 +3,9 @@
 Module for interfacing with the provided CSV file
 
 functions:
+    - export_counter_to_csv
+    - export_tuple_list_to_csv
+    - export_sqlite_rows_to_csv
     - populate_cell_count_csv
 """
 # Imports: Project
@@ -11,7 +14,66 @@ import database
 
 # Imports: Standard Lib
 # ---------------------
+from collections import Counter
 import csv
+import sqlite3
+
+
+# Certainly could have combined and/or abstracted the following three helper functions more, since they all do very
+#   similar actions. export_counter and export_tuple_list especially
+def export_counter_to_csv(csv_output_file: str, header_row: list[str], counter: Counter) -> None:
+    """Export a collections.Counter to a csv file
+
+    Args:
+        csv_output_file (str): path to the output csv file
+        header_row (list[str]): list of strings with the header row values
+        counter (Counter): a collections.Counter object with iterable row information
+
+    Returns:
+        None
+    """
+    with open(csv_output_file, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(header_row)
+
+        for key, count in counter.items():
+            writer.writerow([key, count])
+
+
+def export_tuple_list_to_csv(csv_output_file: str, header_row: list[str], list_data: list[tuple]) -> None:
+    """Export a list of tuples to a csv file
+
+    Args:
+        csv_output_file (str): path to the output csv file
+        header_row (list[str]): list of strings with the header row values
+        list_data (list[tuple]): list of tuples with the row information
+
+    Returns:
+        None
+    """
+    with open(csv_output_file, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(header_row)
+
+        writer.writerows(list_data)
+
+
+def export_sqlite_rows_to_csv(csv_output_file: str, sqlite_rows: list[sqlite3.Row]) -> None:
+    """Export a list of sqlite3 rows returned from cursor.fetchall() to a csv file
+
+    Args:
+        csv_output_file (str): path to the output csv file
+        sqlite_rows (list[sqlite3.Row]): list of sqlite3.Row objects
+
+    Returns:
+        None
+    """
+    with open(csv_output_file, "w", newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=sqlite_rows[0].keys())
+        writer.writeheader()
+
+        for row in sqlite_rows:
+            writer.writerow(dict(row))
 
 
 def populate_cell_count_csv(csv_data_file: str = "cell-count.csv") -> None:
--- load_data.py
+++ load_data.py
@@ -13,18 +13,20 @@ import schema
 def main():
     # part 0
     database.reset_db()
+
     # part 1
-    ## create database
     database.create_table(schema.create_cell_count_table)
-    ## initialize database with data
     csv_helper.populate_cell_count_csv()
+
     # part 2
-    ## frequency of each cell type in each sample
-    ## each row is one population from one sample
     database.create_table(schema.create_cell_relative_frequency_table)
     analysis.populate_relative_frequency_table()
+
     # part 3
+    # TODO
+
     # part 4
+    analysis.query_melanoma_miraclib_baseline()
 
 
 if __name__ == "__main__":
